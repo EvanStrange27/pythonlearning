71ffba3a19d84333fa4ca55e9529852bc06435b3
OutPC <Ivan1019@yeah.net>
2017-11-05 09:49:43 +0800

    add someyhing new

---
+++ b2/four_ways_eight.py
@@ -0,0 +1,5 @@
+# print 8 in four different ways
+print(5 + 3)
+print(2 * 4)
+print(16 // 2)
+print(10 - 2)
---
+++ b2/own_message.py
@@ -0,0 +1,12 @@
+name = "Yang Duck"
+print("Hello " + name + ", welcome to the world!")
+print(name.title())
+print(name.upper())
+print(name.lower())
+message = name.title() + ' said, "If I can, why not?"'
+print(message)
+name = "  \n\tyang duck \n"
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
